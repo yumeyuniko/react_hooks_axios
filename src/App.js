0b90b564e5432fb62de260942d077b7c8399a92a
yumeyuniko <daigocafe@msn>
2022-02-26 18:18:05 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import Jokes from "./components/Jokes";
+import Posts from "./components/Posts";
+
+function App() {
+  return (
+    <main className="App">
+      <h1>useAxios Hooks</h1>
+      <Posts />
+      </main>
+      
+  );
+}
+
+export default App;
